e0d328af46f0b5d5cf35b27f77a7dccf219a8172
EneaSema <sema.enea1993@gmail.com>
2025-05-29 15:06:49 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App() {
+  const urlPosts = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+
+  return (
+    <>
+      <div className="container">
+        <h1>Post Form</h1>
+        <form className="form-post">
+          <label htmlFor="">
+            <h3>Autore del post</h3>
+            <input type="text" placeholder="post-author" />
+          </label>
+          <label htmlFor="">
+            <h3>Titolo del post</h3>
+            <input type="text" placeholder="post-title" />
+          </label>
+          <label htmlFor="">
+            <h3>Contenuto del post</h3>
+
+            <textarea name="post-body" id="">
+              ciao
+            </textarea>
+          </label>
+          <label htmlFor="">
+            <h3>Condividi il post</h3>
+            <input type="checkbox" />
+          </label>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
