34af0a52a35f6bf0868874eca8a6e60978d2509a
EneaSema <sema.enea1993@gmail.com>
2025-05-29 18:44:47 +0200

    resolve-problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,71 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const urlPosts = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+
 function App() {
-  const urlPosts = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  });
+  console.log(formData);
 
-  const [postRiceved, setPostRiceved] = useState([
-    {
-      author: "",
-      title: "",
-      body: "",
-      public: "",
-      id: 0,
-    },
-  ]);
+  ("author");
 
-  useEffect(() => {
-    axios.post(urlPosts).then((response) => {
-      console.log(response);
-      setPostRiceved(response.data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.post(urlPosts).then((response) => {
+  //     console.log(response);
+  //     setFormData(response.data);
+  //   });
+  // }, []);
 
   return (
     <>
       <div className="container">
         <h1>Post Form</h1>
-        <form className="form-post" onSubmit={""} name="">
+        <h1>{JSON.stringify(formData)}</h1>
+        <form
+          className="form-post"
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log("clicked submit");
+          }}
+          name=""
+        >
           <label htmlFor="post-author">
             <h3>Autore del post</h3>
+
             <input
+              onChange={(e) => {
+                const newValue = e.target.value;
+                setFormData({
+                  ...formData,
+                  author: newValue,
+                });
+              }}
+              required
               type="text"
               placeholder="post-author"
-              value={postRiceved.author}
+              value={formData.author}
             />
           </label>
 
-          <label htmlFor="">
-            <h3>Codice identificativo</h3>
-            <input
-              type="number"
-              placeholder="post-title"
-              value={postRiceved.id}
-            />
-          </label>
           <label htmlFor="post-title">
             <h3>Titolo del post</h3>
             <input
+              onChange={(e) => {
+                const newValue = e.target.value;
+                setFormData({
+                  ...formData,
+                  title: newValue,
+                });
+              }}
+              required
               type="text"
               placeholder="post-title"
-              value={postRiceved.title}
+              value={formData.title}
             />
           </label>
           <label htmlFor="post-body">
@@ -57,16 +74,37 @@ function App() {
             <textarea
               cols="100"
               rows="10"
+              onChange={(e) => {
+                const newValue = e.target.value;
+                setFormData({
+                  ...formData,
+                  body: newValue,
+                });
+              }}
               className="text-area-post-body"
               name="post-body"
-              id=""
               placeholder="inserisci il testo del post"
-              value={postRiceved.body}
+              required
+              value={formData.body}
             ></textarea>
           </label>
           <label htmlFor="post-public">
             <h3>Post pubblicato</h3>
-            <input type="checkbox" onClick={""} name={postRiceved.public} />
+            <input
+              onChange={(e) => {
+                const newValue = e.target.value;
+                setFormData({
+                  ...formData,
+                  public: newValue,
+                });
+              }}
+              type="checkbox"
+              onClick={(event) => {
+                event.preventDefault();
+                console.log("clicked submit");
+              }}
+              name={formData.public}
+            />
           </label>
           <button className="btn">Condividi</button>
         </form>
